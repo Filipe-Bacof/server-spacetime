13b39e28cfe83e68faa7dcbf28d95091b2185aa2
Filipe Bacof <filipebacof@gmail.com>
2023-05-17 19:36:24 -0300

    plugin do cors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,12 @@
 import fastify from 'fastify'
+import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
 
+app.register(cors, {
+  origin: true, // todas as URLs de front-end poderão acessar o nosso backend
+})
 app.register(memoriesRoutes)
 
 app
@@ -12,5 +16,3 @@ app
   .then(() => {
     console.log('🔥 Server Running on http://localhost:3333')
   })
-
-// 26:48
